b45ca9e24cfd3fc6ace9ad291fb4387add1ab24c
Ankit <ayushankit2009@gmail.com>
2020-08-11 17:49:06 +0530

    unused var

--- yourOrderPlease.py
+++ yourOrderPlease.py
@@ -6,7 +6,7 @@ Eg., "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
 
 import re
 def order(sentence):
-    final = ''
+    
     sentence = sentence.strip().split()
     result = sorted(sentence, key=lambda str: re.findall("\d+", str)) #regular expression to get int in str
     return   ' '.join(result)
