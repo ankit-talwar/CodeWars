8b116ba323a6ab560a3c42aebd4b44951b7e73ce
Ankit <ayushankit2009@gmail.com>
2020-08-12 16:35:33 +0530

    intial commit

---
+++ SumOfDigits.py
@@ -0,0 +1,16 @@
+"""
+Illustration
+942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+"""
+
+def digital_root(n):
+    sum = 0
+    #making our int n iterable
+    for no in str(n):
+        sum += int(no) 
+    while sum >10:
+        sum = digital_root(sum)
+    return sum
+
+test = digital_root(942)
+print(test)
